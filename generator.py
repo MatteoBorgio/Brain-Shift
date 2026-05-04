0d6f47f30de1b666e3ebf94a6b58f7dd620490b5
matteo.borgio <matteo6.borgio@gmail.com>
2026-05-04 10:37:44 +0200

    fixa generazione della lettera nel trial, dando 50% di possibilità che sia vocale e 50% che sia consonante

--- generator.py
+++ generator.py
@@ -1,12 +1,17 @@
 from models import Trial
 from rules import compute_expected_answer
+from random import choices
 
 
 def generate_trial(rng) -> Trial:
     capital_vowels = ["A", "E", "I", "O", "U"]
+    capital_consonant =  ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M',
+              'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
+    possibile_consonant = choices(capital_consonant, k=5)
+    possible_letter = capital_vowels + possibile_consonant
     possible_positions = ["TOP", "BOTTOM"]
     position = rng.choice(possible_positions)
-    letter = rng.choice(capital_vowels)
+    letter = rng.choice(possible_letter)
     number = rng.randint(1, 9)
     expected_answer = compute_expected_answer(position, letter, number)
     return Trial(
